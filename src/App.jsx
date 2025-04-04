fa1a61fd984e903e699f9157e275794791364fc0
tibzloll <148633790+tibzloll@users.noreply.github.com>
2025-04-04 20:49:00 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Watch from './pages/Watch';
 
 const App = () => {
+  useEffect(() => {
+    console.log('App mounted - check network requests');
+  }, []);
+
   return (
-    <Router>
+    <div style={{ color: 'white', padding: '2rem' }}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/watch/:mediaType/:id" element={<Watch />} />
       </Routes>
-    </Router>
+    </div>
   );
 };
 
